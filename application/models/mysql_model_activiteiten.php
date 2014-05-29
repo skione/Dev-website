e397ffd118cde4fffba120db94d8d3bf503de421
Michael Sole <mfsole@hotmail.com>
2014-05-29 16:26:35 -0400

    Fixed problem

--- application/models/mysql_model_activiteiten.php
+++ application/models/mysql_model_activiteiten.php
@@ -5,12 +5,7 @@
 		function Kapoenen_activiteiten() {
 			$sql_activiteit= "SELECT * FROM Activiteiten WHERE Tak = 'Kapoenen'";
 			$query_activiteit = $this->db->query($sql_activiteit);
-			if($query_activiteit->num_rows() > 0) {
-				foreach ($query_activiteit->result() as $row) {
-					$data[] = $row;
-				}
-				return $data;
-			}
+			return $query_activiteit->result()
 		}
 
 		function Welpen_activiteiten() {
